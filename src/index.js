3e430e3119d9dea85d8f729133d4b6fbd5d55daa
Artsiom Tratsiuk <inqui05@gmail.com>
2021-01-26 21:22:17 +0300

    implemented 3 function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array === undefined) {
+    return 0;
+  } else {
+    let min = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+    }
+
+    return array.length > 0 ? min : 0;
+  }
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array === undefined) {
+    return 0;
+  } else {
+    let max = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+
+    return array.length > 0 ? max : 0;
+  }
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  let sum = 0;
+
+  if (array === undefined) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+  }
+
+  return array.length > 0 ? sum/array.length : 0;
+};
